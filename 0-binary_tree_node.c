0ace6b62838393ec1a3da062339cc817baefa87e
mkl-74 <mickael1008@gmail.com>
2024-01-08 12:50:27 +0000

    0-binary-comment

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,11 +4,12 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_node - Create a binary tree node.
+ * @parent : pointer to the parent of the node create
+ * @value : Value input a new node
  *
  *
- *
- *
- *
+ * Return : Error = NULL / pointer to a new node
  *
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
