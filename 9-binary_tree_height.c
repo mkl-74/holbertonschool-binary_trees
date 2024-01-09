efafc2eb96e5e23cbfc0f9fa1b4ad8ceb429414a
mkl-74 <mickael1008@gmail.com>
2024-01-09 10:51:26 +0000

    modif indentation

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree.
- *
- * Return: The height of the tree. If tree is NULL, return 0.
- */
+* binary_tree_height - Measures the height of a binary tree.
+* @tree: A pointer to the root node of the tree.
+*
+* Return: The height of the tree. If tree is NULL, return 0.
+*/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
